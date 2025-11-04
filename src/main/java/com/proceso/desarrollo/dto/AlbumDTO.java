5ba7f38553815e59b52bd54567afafebaf971aa3
tomylasalde <tomylasalde@gmail.com>
2025-11-03 23:45:21 -0300

    fix: update AlbumDTO id type to Long and add null check in AlbumController

--- src/main/java/com/proceso/desarrollo/dto/AlbumDTO.java
+++ src/main/java/com/proceso/desarrollo/dto/AlbumDTO.java
@@ -2,7 +2,7 @@ package com.proceso.desarrollo.dto;
 
 import java.time.LocalDate;
 
-public record AlbumDTO(long id, String titulo, String descripcion, boolean publicado, LocalDate fechaCreacion,
+public record AlbumDTO(Long id, String titulo, String descripcion, boolean publicado, LocalDate fechaCreacion,
 		int totalFiguritas) {
 
 }
